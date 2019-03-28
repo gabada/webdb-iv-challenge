949d45736d4ea9dfa2cdd8ba1325d9df31a4eec9
Gill Abada <gill@callifi.com>
2019-03-28 18:31:05 -0400

    add dishes router.

---
+++ dishes/dishes-model.js
@@ -0,0 +1,25 @@
+const db = require('../data/dbConfig.js');
+
+module.exports = {
+  find,
+  findById,
+  addDish
+};
+
+function find() {
+  return db('dishes');
+}
+
+function findById(id) {
+  return db('dishes')
+    .where({ id })
+    .first();
+}
+
+function addDish(dish) {
+  return db('dishes')
+    .insert(dish)
+    .then(ids => {
+      return findById(ids[0]);
+    });
+}
--- index.js
+++ index.js
@@ -5,8 +5,12 @@ const helmet = require('helmet');
 const server = express();
 const port = 5000;
 
+const dishesRouter = require('./dishes/dishes-router.js');
+
 server.use(express.json());
 server.use(cors());
 server.use(helmet());
+
+server.use('/api/dishes', dishesRouter);
 
 server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
